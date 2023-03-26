91feeba1845fe750ee947733a099e4d7e08b93d1
Thomas Beyene <thomas.beyene.987@gmail.com>
2023-03-26 19:43:25 +0300

    Updated version of puppet file

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,12 +1,12 @@
 # Using puppet, configured
 # SSH client configuration to use the private key ~/.ssh/school
 # SSH client configuration refuse to authenticate using a password
-file_line { 'config_passwd':
+file_line { 'Turn_off_passwd_auth':
   ensure => present,
   path   => '/etc/ssh/ssh_config',
-  match  => '    PasswordAuthentication no',
+  line  => '    PasswordAuthentication no',
 }
-file_line { 'config_privateKey':
+file_line { 'Declare_identity_file':
   ensure => present,
   path   => '/etc/ssh/ssh_config',
   line   => '    IdentityFile ~/.ssh/school',
