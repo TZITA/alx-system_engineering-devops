74548613cec7adffc3f979b0c760bc3cccd20f4b
Thomas Beyene <thomas.beyene.987@gmail.com>
2023-03-28 22:16:53 +0300

    fixes a syntax error in the file block

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -3,10 +3,6 @@
 #	- string "Hello World!" upon GET request by curl
 #	- a redirection 301 Moved Permanently
 
-package { 'nginx':
-  ensure => installed,
-}
-
 file { '/etc/nginx/sites-available/default':
   content => "server {
       listen 80;
@@ -15,11 +11,6 @@ file { '/etc/nginx/sites-available/default':
       index index.html index.htm;
       location /redirect_me { 
           return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
-    }
-      error_page 404 /404.html;
-      location /404 {
-          root /etc/nginx/html;
-          internal;
     }",
   notify  => Service['nginx'],
 }
