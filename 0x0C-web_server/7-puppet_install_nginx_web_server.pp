df9c1b94c9f08cd91d66027d198de6a0e0263c95
Thomas Beyene <thomas.beyene.987@gmail.com>
2023-03-28 22:12:35 +0300

    fixes a syntax error in the file block

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -10,10 +10,16 @@ package { 'nginx':
 file { '/etc/nginx/sites-available/default':
   content => "server {
       listen 80;
+      listen [::]:80 default_server;
       root /etc/nginx/html;
       index index.html index.htm;
-      location /redirect_me {
+      location /redirect_me { 
           return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
+    }
+      error_page 404 /404.html;
+      location /404 {
+          root /etc/nginx/html;
+          internal;
     }",
   notify  => Service['nginx'],
 }
