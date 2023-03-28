727633a51763dad04787f3a2059a55a32810f600
Thomas Beyene <thomas.beyene.987@gmail.com>
2023-03-28 22:36:50 +0300

    fixes a syntax error in the file block

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -2,6 +2,9 @@
 #	- Nginx should be listening on port 80
 #	- string "Hello World!" upon GET request by curl
 #	- a redirection 301 Moved Permanently
+package { 'nginx':
+  ensure => installed,
+}
 
 file { '/etc/nginx/sites-available/default':
   ensure  => file,
@@ -11,7 +14,8 @@ file { '/etc/nginx/sites-available/default':
       listen [::]:80 default_server;
       root /etc/nginx/html;
       index index.html index.htm;
-      return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
+      location /redirect_me {
+          return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
     }",
   notify  => Service['nginx'],
 }
