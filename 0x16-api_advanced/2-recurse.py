be24515fab70a1465b5eb0473d8f89c7f4fb9a60
Thomas Beyene <thomas.beyene.987@gmail.com>
2023-05-09 22:20:21 +0300

    Task 2

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,24 +1,26 @@
 #!/usr/bin/python3
-"""xxxxxxx x xxxxxxx xxxxxxxxxx"""
+"""Recursive function to list all titles of hot articles"""
 import requests
 
 
 def recurse(subreddit, after=None):
-    """ccccccccc cccccccccccccccc cccccccccc cc ccc cccc ccccccccc"""
+    """
+    Returns a list of titles of all hot articles for a given subreddit.
+    """
     headers = {'User-Agent': 'tzita/v1.0'}
     url = f'https://www.reddit.com/r/{subreddit}/hot.json?limit=100'
     if after:
         url += f'&after={after}'
     response = requests.get(url, headers=headers)
-    if response.status_code == 200:
-        data = response.json()
-        posts = data['data']['children']
-        if not posts:
-            return []
-        else:
-            titles = [post['data']['title'] for post in posts]
-            after = data['data']['after']
-            remaining_titles = recurse(subreddit, after)
-            return titles + remaining_titles
-    else:
+    if response.status_code != 200:
         return None
+    data = response.json()['data']
+    if not data['children']:
+        return None
+    posts = [post['data']['title'] for post in data['children']]
+    next_page = data['after']
+    if next_page:
+        more_posts = recurse(subreddit, next_page)
+        if more_posts:
+            posts += more_posts
+    return posts
--- 0x16-api_advanced/README.md
+++ 0x16-api_advanced/README.md
@@ -7,3 +7,7 @@ A function that queries the Reddit API and returns the number of subscribers (no
 ## Task 1:
 ## Top Ten
 A function that queries the Reddit API and prints the titles of the first 10 hot posts listed for a given subreddit. If not a valid subreddit, print None.
+
+## Task 2:
+## Recurse it!
+A recursive function that queries the Reddit API and returns a list containing the titles of all hot articles for a given subreddit. If no results are found for the given subreddit, the function should return None.
