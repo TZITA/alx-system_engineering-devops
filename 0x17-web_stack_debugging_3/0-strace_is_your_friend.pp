025f1af4c9e0fcd2b1275b8f0e2fafc986a1071a
Thomas Beyene <thomas.beyene.987@gmail.com>
2023-05-11 00:11:18 +0300

    Init commit

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,6 @@
+# A puppet manifest that fixes a bug (spelling error) in the file wp-settings.php. The change is from .phpp to .php
+
+exec { 'fix wp':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => 'usr/local/bin/'
+}
---
+++ 0x17-web_stack_debugging_3/README.md
@@ -0,0 +1,6 @@
+#  Web stack debugging #3
+
+## Task 0:
+## Strace is your friend
+
+Using strace, find out why Apache is returning a 500 error. Once the issue is found, fix it and then automate it using Puppet.
