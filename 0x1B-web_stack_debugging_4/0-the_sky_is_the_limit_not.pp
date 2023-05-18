e5e49781d19b396f6c34ec1cb51cddacf4c37e4f
Thomas Beyene <thomas.beyene.987@gmail.com>
2023-05-18 17:54:48 +0300

    fixed puppet-lint error task 0

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,8 +1,8 @@
 # A puppet manifest to config nginx and sys config files prevent too many open files error
 exex { 'fix--for-nginx':
   command => 'sed -E -i "s/ULIMIT=\"-n [[:digit:]]+\"/ULIMIT=\"-n 4096\"/" /etc/default/nginx; \
-	      sed -E -i "s/* soft nofile [[:digit:]]+/* soft nofile 65536/" /etc/security/limits.conf; \
-              sed -E -i "s/* hard nofile [[:digit:]]+/* hard nofile 65536/" /etc/security/limits.conf'; \
+	      sed -E -i "s/\* soft nofile [[:digit:]]+/\* soft nofile 65536/" /etc/security/limits.conf; \
+              sed -E -i "s/\* hard nofile [[:digit:]]+/\* hard nofile 65536/" /etc/security/limits.conf; \
 	      sed -i "3iworker_rlimit_nofile 30000;"0 /etc/nginx/nginx.conf',
   path    => ['usr/bin', '/bin'],
   notify  => Service['nginx'],
