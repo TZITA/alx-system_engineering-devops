265086e44d8bdbf2937a15a367af9d191999220e
Thomas Beyene <thomas.beyene.987@gmail.com>
2023-05-19 17:29:15 +0300

    t0 updated6

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,9 +1,6 @@
 # A puppet manifest to config nginx and sys config files prevent too many open files error
 exec { 'fix--for-nginx':
-  command => 'sed -E -i "s/ULIMIT=\"-n [[:digit:]]+\"/ULIMIT=\"-n 4096\"/" /etc/default/nginx; \
-	      sed -E -i "s/\* soft nofile [[:digit:]]+/\* soft nofile 65536/" /etc/security/limits.conf; \
-              sed -E -i "s/\* hard nofile [[:digit:]]+/\* hard nofile 65536/" /etc/security/limits.conf; \
-	      sed -i "3iworker_rlimit_nofile 30000;"0 /etc/nginx/nginx.conf',
+  command => 'sed -E -i "s/ULIMIT=\"-n [[:digit:]]+\"/ULIMIT=\"-n 4096\"/" /etc/default/nginx',
   path    => ['usr/bin', '/bin'],
   notify  => Service['nginx'],
 }
