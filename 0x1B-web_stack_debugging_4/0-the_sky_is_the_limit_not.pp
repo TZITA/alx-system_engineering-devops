9ec57d927c470aa6260cf5ce59aeb6d57f713d92
Thomas Beyene <thomas.beyene.987@gmail.com>
2023-05-19 16:58:43 +0300

    t0 updated1

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,7 +1,7 @@
 # A puppet manifest to config nginx and sys config files prevent too many open files error
 exex { 'fix--for-nginx':
   command => 'sed -E -i "s/ULIMIT=\"-n [[:digit:]]+\"/ULIMIT=\"-n 4096\"/" /etc/default/nginx',
-  path    => ['usr/bin', '/bin'],
+  path    => ['usr/local/bin:/bin'],
   notify  => Service['nginx'],
 }
 
